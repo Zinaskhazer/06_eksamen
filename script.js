d94b23859b28ce9d0ee039536046e8d06cc6e976
Zina <zikh001@kea.dk>
2024-06-21 21:53:45 +0200

    HEEEEEJ

--- script.js
+++ script.js
@@ -8,35 +8,3 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "projekter.html";
   });
 });
-
-// seprojektknap til tema 2 //
-
-document.addEventListener("DOMContentLoaded", () => {
-  seProjekt1.addEventListener("click", () => {
-    window.location.href = "http://zinasaleh.dk/kea/02_web/01_website/website/galleri.html";
-  });
-});
-
-// seprojektknap til tema 3 //
-
-document.addEventListener("DOMContentLoaded", () => {
-  seProjekt2.addEventListener("click", () => {
-    window.location.href = "http://zinasaleh.dk/kea/03_ux/01_kodet_site/index.html";
-  });
-});
-
-// seprojektknap til tema 4 //
-
-document.addEventListener("DOMContentLoaded", () => {
-  seProjekt3.addEventListener("click", () => {
-    window.location.href = "http://zinasaleh.dk/kea/04_animation/website/spil.html";
-  });
-});
-
-// seprojektknap til tema 5 //
-
-document.addEventListener("DOMContentLoaded", () => {
-  seProjekt4.addEventListener("click", () => {
-    window.location.href = "https://zinasaleh.dk/05_indhold/centralhotelogcafe/about.html";
-  });
-});
