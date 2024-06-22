a4eee756a2889b15862b5d2fb3a4e5a4b4d05630
Zina <zikh001@kea.dk>
2024-06-22 12:53:53 +0200

    yay

--- script.js
+++ script.js
@@ -8,3 +8,39 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "projekter.html";
   });
 });
+
+// TEMA 2 KNAP//
+const setema2 = document.querySelector("#tema2");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setema2.addEventListener("click", () => {
+    window.location.href = "http://zinasaleh.dk/kea/02_web/01_website/website/";
+  });
+});
+
+// TEMA 3 KNAP //
+const setema3 = document.querySelector("#tema3");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setema3.addEventListener("click", () => {
+    window.location.href = "http://zinasaleh.dk/kea/03_ux/01_kodet_site/index.html";
+  });
+});
+
+// TEMA 4 KNAP//
+const setema4 = document.querySelector("#tema4");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setema4.addEventListener("click", () => {
+    window.location.href = "http://zinasaleh.dk/kea/04_animation/website/spil.html";
+  });
+});
+
+// TEMA 5 KNAP//
+const setema5 = document.querySelector("#tema5");
+
+document.addEventListener("DOMContentLoaded", () => {
+  setema5.addEventListener("click", () => {
+    window.location.href = "https://zinasaleh.dk/05_indhold/centralhotelogcafe/about.html";
+  });
+});
